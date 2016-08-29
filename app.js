072c8c081558002abd4b508b4dce62f4b3abe1f9
Chaoyue <hechaoyue0307@gmail.com>
2016-08-29 17:18:05 +0800

    NodeJS Practe script

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by user1 on 29/08/2016.
+ */
+
+function placeAnOrder(orderNumber){
+    console.log('Customer order:', orderNumber);
+    cookAndDeliverFood(function (){
+        console.log('Delvered food order:', orderNumber);
+    })
+}
+
+//Simulate a 5 second operation
+function cookAndDeliverFood(callback){
+    setTimeout(callback,5000);
+}
+
+//Simulate users web requests
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
+placeAnOrder(6);
